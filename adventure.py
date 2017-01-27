ab18abd837e52241e842ea57cdd7f0b270b8612a
juan-david1025 <juandavidguerra1@gmail.com>
2017-01-27 12:59:56 -0500

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -15,6 +15,7 @@
 #Type of game - Choice based game, you control the main character
 #--------------------------------------------------------------------------
 
+#loading the doc to travis-ci
 [![Build Status](https://travis-ci.org/juan-david1025/Adventure_Game.svg?branch=master)](https://travis-ci.org/juan-david1025/Adventure_Game) 
 
 # Imports
